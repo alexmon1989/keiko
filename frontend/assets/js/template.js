575c2f049edd096684a942b603f6e366a26e14e1
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2019-02-05 17:31:01 +0200

    работа по шаблону.

--- frontend/assets/js/template.js
+++ frontend/assets/js/template.js
@@ -1,7 +1,10 @@
 // Styles
 import '../vendor/bootstrap/bootstrap.min.css';
 import '../include/scss/unify.scss'
+import '../include/scss/custom.scss'
 
 // Scripts
 import '../vendor/jquery-migrate/jquery-migrate.min.js';
 import '../vendor/bootstrap/bootstrap.min.js';
+
+import './hs.core.js';
