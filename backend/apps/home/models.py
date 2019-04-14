95d4fdbbbf85bfc03aba0de474aecc7aebaa44ac
Alex Monastyretsky <alex.mon1989@gmail.com>
2019-04-14 22:15:31 +0300

    fix displaying error in admin panel.

--- backend/apps/home/models.py
+++ backend/apps/home/models.py
@@ -13,7 +13,7 @@ class Slide(TimeStampedModel):
     is_enabled = models.BooleanField('Включено', default=True)
 
     def __str__(self):
-        return self.pk
+        return str(self.pk)
 
     class Meta:
         verbose_name = 'Слайд'
