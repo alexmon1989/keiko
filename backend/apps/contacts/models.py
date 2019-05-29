360742f65fe7515e7f61009fa232343389b7a029
Alex Monastyretsky <alex.mon1989@gmail.com>
2019-05-29 22:59:53 +0300

    wysiwyg editor for admin contacts page

--- backend/apps/contacts/models.py
+++ backend/apps/contacts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 from keiko.utils import SeoModel
 
 
 class ContactsModel(SeoModel):
     map_code = models.TextField('Код карты (HTML)', default='')
-    contacts_code = models.TextField('Код контактных данных (HTML)', default='')
+    contacts_code = RichTextField('Контактные данные', default='')
 
     class Meta:
         verbose_name = 'Контакты'
